a4b7dad7ad0ac9a7c53dd0efca811aded712fbe3
magui165-byte <113001565+magui165-byte@users.noreply.github.com>
2024-09-19 01:27:42 -0400

    finished portfolio and curriculum

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,23 +1,34 @@
 import React from 'react'
 import { works } from '../data/works'
+import { Link } from 'react-router-dom'
+
 
 export const Portfolio = () => {
   return (
     <div className='page'>
       <h1>Portfolio</h1>
+      <section className='works'>
       {
         works.map(work => {
           return(
-            <article key={work.id} className='works'>
-              
+            <article key={work.id} className='work-item'> 
               <h2>{work.name}</h2>
-              <p>{work.description}</p>
+              <div className='mask'>
+                <img src={'/images/'+work.id+'.gif' } ></img>
+              </div>
+              
+              
+              <h3>{work.description}</h3>
               <span>Tags: {work.tags}</span>
-              <p>url: {work.url}</p>
+              <h3> <Link to={work.url}>Github Repository</Link></h3>
+              <hr></hr>
             </article>
           )
         })
       } 
+      </section>
+      
     </div>
   )
 }
+
